148d00b7782df8f48d1972a0d64fa0a8107b0c19
jeanfcg <104164533+jeanfcg@users.noreply.github.com>
2025-12-02 23:39:26 -0500

    Update addone.cu

--- cuda/addone.cu
+++ cuda/addone.cu
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cuda_runtime.h>
 
-// Kernel en GPU: cada hilo suma 1 a un elemento
+//kernel en GPU: cada hilo suma 1 a un elemento
 __global__ void addOne(int *data, int n) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < n) {
@@ -13,30 +13,25 @@ int main() {
     int n = 10;
     int size = n * sizeof(int);
 
-    // Crear arreglo en CPU
+    //crear arreglo en CPU
     int h_data[10];
     for (int i = 0; i < n; i++) h_data[i] = i;
 
-    // Reservar memoria en GPU
+    //reservar memoria en GPU
     int *d_data;
     cudaMalloc((void**)&d_data, size);
-
-    // Copiar datos CPU → GPU
-    cudaMemcpy(d_data, h_data, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_data, h_data, size, cudaMemcpyHostToDevice); //copiar datos del cpu al gpu
 
     // Lanzar kernel: 1 bloque de 10 hilos
     addOne<<<1, 10>>>(d_data, n);
+    cudaMemcpy(h_data, d_data, size, cudaMemcpyDeviceToHost); //copiar el resultado del gpu al cpu
 
-    // Copiar resultado GPU → CPU
-    cudaMemcpy(h_data, d_data, size, cudaMemcpyDeviceToHost);
-
-    // Mostrar resultado
     for (int i = 0; i < n; i++)
         std::cout << h_data[i] << " ";
     std::cout << std::endl;
 
-    // Liberar memoria
     cudaFree(d_data);
 
     return 0;
+
 }
